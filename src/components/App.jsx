89ab8c9f889ceedb7be41096a9a41c09158ed33f
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-07-28 22:19:49 +0300

    removed mockapi fake backend to another repo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world!
         </p>
         <a
           className="App-link"
