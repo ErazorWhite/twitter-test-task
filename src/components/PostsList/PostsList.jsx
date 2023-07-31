2c6eda1655611ed09cbd70567a366d260ef45855
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-07-31 22:15:18 +0300

    console log removed

--- src/components/PostsList/PostsList.jsx
+++ src/components/PostsList/PostsList.jsx
@@ -4,7 +4,6 @@ import { Ul } from './PostsList.styled';
 export const PostsList = ({ posts }) => {
   return (
     <Ul>
-      {console.log('PostsList: ', posts)}
       {posts &&
         posts.map(post => <PostItem key={post.messageId} postDetails={post} />)}
     </Ul>
