3eee5b84248b667c4b5ccb51dd9c87f67676316c
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-08-02 02:57:15 +0300

    backdrop alpha channels now is a bit darker

--- src/components/Backdrop/Backdrop.styled.js
+++ src/components/Backdrop/Backdrop.styled.js
@@ -6,6 +6,6 @@ export const BackdropContainer = styled.div`
   left: 0;
   right: 0;
   bottom: 0;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: rgba(0, 0, 0, 0.6);
   z-index: 1000;
 `;
