5913cd3363e8a56afa92fd4df1eb289d10ee270f
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2024-01-04 17:43:15 +0200

    path fix for deployment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,9 @@ import { Layout } from 'Layout/Layout';
 import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-const Posts = lazy(() => import('../pages/Posts'));
-const Profile = lazy(() => import('../pages/Profile'));
-const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
+const Posts = lazy(() => import('pages/Posts'));
+const Profile = lazy(() => import('pages/Profile'));
+const NotFoundPage = lazy(() => import('pages/NotFoundPage'));
 
 function App() {
   return (
